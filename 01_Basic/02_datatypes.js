629c33b27c2319c09c7102526bd52668a5127aa4
Gautam Mehar <133129329+Matuag28@users.noreply.github.com>
2025-09-05 06:44:16 +0000

    New file added

---
+++ 01_Basic/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code  as newer version
+
+// alert(hello)  // we are using node js not browser 
+
+// code readability should be high
+
+
+//  ---------------------------------------- Data types------------------------------------------
+// String => ""
+// Boolean => true/false
+// Number => 18
+// BigInt => 
+// null => standalone variable
+// symbol => unique
+
+//  null is a object  and undefined is undefined
+
