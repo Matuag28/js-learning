00547a380c2490e6627140145672de1b19d24b37
Gautam Mehar <133129329+Matuag28@users.noreply.github.com>
2025-09-05 07:04:58 +0000

    03 file added

---
+++ 01_Basic/03_datatype_Conversion.js
@@ -0,0 +1,40 @@
+let score = 'GG'
+console.log(score);
+console.log(typeof(score));
+
+
+let conver =String(score)
+console.log(conver);
+console.log(typeof conver);
+
+let conv = Boolean(score)
+console.log(conv);
+
+let co = Number(score)
+console.log(co);
+
+let isLoggedIn =1
+let BooleanisLoggedIn=Boolean(isLoggedIn)
+console.log(BooleanisLoggedIn);
+
+// console.log( typeof BooleanisLoggedIn);
+
+
+
+/*
+nan = NotaNumber
+"33" => 33
+"33abc" => NaN
+ true => 1
+ false => 0
+
+
+ 1 => true
+ 0 => false
+
+ "GG" => true
+ 
+
+
+
+*/
