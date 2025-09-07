aba91d1ae06e6da593787e46efad5c222ac640b8
Gautam Mehar <133129329+Matuag28@users.noreply.github.com>
2025-09-07 10:38:32 +0000

    dataTypes summary

--- 01_Basic/datatypesSummary.js
+++ 01_Basic/datatypesSummary.js
@@ -6,3 +6,17 @@
 
 // Array , objects , Functions 
 
+const id =Symbol('123')
+const andotherId = Symbol('123')
+
+console.log(id === andotherId);
+
+const myFunction = function(){
+    console.log("Hello");
+}
+
+let obj={
+    name:'HH',
+    age:28
+}
+console.log(typeof obj);
